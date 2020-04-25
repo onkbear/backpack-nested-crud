c810c8eac69d88314017ed70c00e76926f349dca
onkbear <onkbear@gmail.com>
2020-04-25 23:46:49 +0100

    Fixed delete button - do not use $crud->request

--- src/resources/views/nested_crud/buttons/delete.blade.php
+++ src/resources/views/nested_crud/buttons/delete.blade.php
@@ -5,7 +5,7 @@
 {{-- Button Javascript --}}
 {{-- - used right away in AJAX operations (ex: List) --}}
 {{-- - pushed to the end of the page, after jQuery is loaded, for non-AJAX operations (ex: Show) --}}
-@push('after_scripts') @if ($crud->request->ajax()) @endpush @endif
+@push('after_scripts') @if (request()->ajax()) @endpush @endif
 <script>
 
   if (typeof deleteEntry != 'function') {
@@ -88,4 +88,4 @@
   // make it so that the function above is run after each DataTable draw event
   // crud.addFunctionToDataTablesDrawEventQueue('deleteEntry');
 </script>
-@if (!$crud->request->ajax()) @endpush @endif
+@if (!request()->ajax()) @endpush @endif
